ea1c0dd3d22ad65e6c137b6c1e2b20f7733fa05b
charankurru <61787496+charankurru@users.noreply.github.com>
2021-10-14 18:22:58 +0530

    stl added

---
+++ Collections/CollTest.java
@@ -0,0 +1,7 @@
+package java_progs.Collections;
+
+public class CollTest {
+    public static void main(String[] args) {
+        System.out.println("Learning the probability distribution");
+    }
+}
