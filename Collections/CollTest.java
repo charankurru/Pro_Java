8fa5e777b031ea8c8b040d0888fc8707f177b5c6
charankurru <61787496+charankurru@users.noreply.github.com>
2021-10-14 19:45:48 +0530

    done

--- Collections/CollTest.java
+++ Collections/CollTest.java
@@ -1,7 +1,23 @@
 package java_progs.Collections;
 
+import java.util.*;
+
 public class CollTest {
     public static void main(String[] args) {
-        System.out.println("Learning the probability distribution");
+        ArrayList<String> list = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            list.add("roll num:" + Integer.toString(i));
+        }
+        sc.close();
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        // Adding elements at a specific position
+        list.add(1, "roll num 1232");
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
     }
 }
---
+++ SplitExample.java
@@ -0,0 +1,30 @@
+package java_progs;
+
+/**
+ * SplitExample
+ */
+public class SplitExample {
+    public static void main(String[] args) {
+        String str = "I,Love, coding, very, much";
+        String newstr = " ";
+        String finalstr = " ";
+
+        // Code to split a given String on delimiter , //s ==> White Space.
+        String arr[] = str.split(",");
+        for (String str2 : arr) {
+            System.out.println(str2);
+        }
+
+        // Code to reverse each word in a given String
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                finalstr = finalstr + newstr;
+                newstr = " ";
+            }
+            newstr = str.charAt(i) + newstr;
+        }
+        finalstr += newstr;
+        System.out.println(finalstr);
+    }
+}
